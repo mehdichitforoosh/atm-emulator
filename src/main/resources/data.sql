3ce4f4e188e62fd5d347fcb4c1e5f8e3f85f960f
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2021-10-02 14:48:29 +0330

    Add initialization schema, data scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+-- user sample records
+INSERT INTO users (id,first_name,last_name,description) VALUES (1,'Mehdi','Chitforoosh','Senior Java Developer');
+INSERT INTO users (id,first_name,last_name,description) VALUES (2,'ALi','Rezaei','');
+INSERT INTO users (id,first_name,last_name,description) VALUES (3,'Morteza','Kalantar','Senior Frontend Developer');
+INSERT INTO users (id,first_name,last_name,description) VALUES (4,'Ali','Chitforoosh','');
+INSERT INTO users (id,first_name,last_name,description) VALUES (5,'Mohammad','Javadi','');
+-- bank sample records
+INSERT INTO banks (id,name,branch_number,address) VALUES (1,'Melli','110','Iran, Yazd');
+INSERT INTO banks (id,name,branch_number,address) VALUES (2,'Melli','120','Iran, Yazd');
+INSERT INTO banks (id,name,branch_number,address) VALUES (3,'Mellat','1112','Iran, Yazd');
+INSERT INTO banks (id,name,branch_number,address) VALUES (4,'Mellat','1122','Iran, Yazd');
+INSERT INTO banks (id,name,branch_number,address) VALUES (5,'Sepah','10','Iran, Yazd');
+INSERT INTO banks (id,name,branch_number,address) VALUES (6,'Pasargad','3456','Iran, Yazd');
+-- account sample records
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (1,'43897345',102000,1,1);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (2,'43897346',12000,1,2);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (3,'1000534',8000,3,1);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (4,'1000876',92600,3,4);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (5,'28978887',132000,6,5);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (6,'28978957',1000,6,3);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (7,'2000967',38478,5,3);
+INSERT INTO accounts (id,account_number,balance,bank_id,user_id) VALUES (8,'1000345',102000,4,1);
+-- card sample records
+INSERT INTO cards (id,card_number,pin,expiration_date,active,account_id) VALUES (1,'9856342578786543','4567','2022-06-22 19:10:25-07',true,1);
+INSERT INTO cards (id,card_number,pin,expiration_date,active,account_id) VALUES (2,'9856342578325498','9345','2021-12-22 20:10:25-07',true,2);
+INSERT INTO cards (id,card_number,pin,expiration_date,active,account_id) VALUES (3,'8765342578323421','2364','2023-06-22 12:10:25-07',true,5);
+INSERT INTO cards (id,card_number,pin,expiration_date,active,account_id) VALUES (4,'2345879078321112','1178','2021-06-22 19:10:25-07',false,7);
+
+
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,73 @@
+-- Table: users
+CREATE TABLE users
+(
+  id bigint NOT NULL,
+  description character varying(2000),
+  first_name character varying(30) NOT NULL,
+  last_name character varying(50) NOT NULL,
+  CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE users
+  OWNER TO postgres;
+-------------------------------------------------
+-- Table: banks
+CREATE TABLE banks
+  (
+    id bigint NOT NULL,
+    address character varying(200),
+    branch_number character varying(30) NOT NULL,
+    name character varying(30) NOT NULL,
+    CONSTRAINT banks_pkey PRIMARY KEY (id)
+  )
+  WITH (
+    OIDS=FALSE
+  );
+  ALTER TABLE banks
+    OWNER TO postgres;
+-------------------------------------------------
+-- Table: accounts
+CREATE TABLE accounts
+  (
+    id bigint NOT NULL,
+    account_number character varying(30) NOT NULL,
+    balance numeric(19,2) NOT NULL,
+    bank_id bigint,
+    user_id bigint,
+    CONSTRAINT accounts_pkey PRIMARY KEY (id),
+    CONSTRAINT fkb78evw9x9jyy66ld572kl8rgx FOREIGN KEY (bank_id)
+        REFERENCES banks (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT fknjuop33mo69pd79ctplkck40n FOREIGN KEY (user_id)
+        REFERENCES users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+  )
+  WITH (
+    OIDS=FALSE
+  );
+  ALTER TABLE accounts
+    OWNER TO postgres;
+-------------------------------------------------
+-- Table: cards
+CREATE TABLE cards
+(
+  id bigint NOT NULL,
+  card_number character varying(16) NOT NULL,
+  expiration_date timestamp without time zone NOT NULL,
+  pin character varying(4) NOT NULL,
+  active boolean NOT NULL,
+  account_id bigint,
+  CONSTRAINT cards_pkey PRIMARY KEY (id),
+  CONSTRAINT fkdjw7dinkpc0f01yk4m57uq2u2 FOREIGN KEY (account_id)
+      REFERENCES accounts (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE cards
+  OWNER TO postgres;
+
+
